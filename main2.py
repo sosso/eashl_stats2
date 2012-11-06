a3f9355f6f8068247607854630ac3ae96c13315e
Anthony Sosso <asosso@friendeo.com>
2012-11-06 00:57:51 -0500

    gets games played, needs to be cron-ified

--- main2.py
+++ main2.py
@@ -1,26 +1,43 @@
 from BeautifulSoup import BeautifulSoup
 from models import Session, Game, GamePlayed, Player
 import dbutils
+import gspread
 import urllib2 #@UnresolvedImport
+import logging #@UnresolvedImport
 
+logging.basicConfig()
+logger = logging.getLogger('stats')
 
 headers = { 'User-Agent' : 'Mozilla/5.0' }
 base_game_details_url = 'http://www.easportsworld.com/en_US/clubs/partial/401A0001/273/match-results/details?match_id=%s&type=all'
 
 def check_games():
-    games_list_req = urllib2.Request('https://dl.dropbox.com/u/6996716/simple_games_list.htm', None, headers)
-    games_list_html = urllib2.urlopen(games_list_req, None, 30).read()
+#    games_list_req = urllib2.Request('https://dl.dropbox.com/u/6996716/simple_games_list.htm', None, headers)
+    games_list_req = urllib2.Request('http://www.easportsworld.com/en_US/clubs/partial/401A0001/273/match-results/', None, headers)
+
+    try:
+        logger.info('Fetching games list . . .')
+        games_list_html = urllib2.urlopen(games_list_req, None, 30).read()
+    except Exception, e:
+        logger.exception('Timed out fetching games list')
+        games_list_html = ''
     games_list_soup = BeautifulSoup(games_list_html)
 
     games = games_list_soup.findAll('div', id=lambda x: x and x.startswith('match-detail-container'))
-
+    should_update_spreadsheet = False
     for game in games:
-        game_id = game['id'][23:]#strips game id from match-detail-container-36028797029524422
-        game = Session().query(Game).filter(matchid=game_id).first()
-        if game is None:
-            process_game(game_id)
-        else: #we have already processed the game, ignore it
-            continue
+        try:
+            game_id = game['id'][23:]#strips game id from match-detail-container-36028797029524422
+            game = Session().query(Game).filter_by(match_id=game_id).first()
+            if game is None:
+                logger.info('New game detected %s; archiving. . .' % game_id)
+                process_game(game_id)
+                should_update_spreadsheet = True
+            else: #we have already processed the game, ignore it
+                logger.debug('Already archived this game')
+                pass
+        except:
+            pass
 
 def process_game(game_id):
     game_url = base_game_details_url % game_id
@@ -36,9 +53,42 @@ def process_game(game_id):
         session.flush()
         for teammate in guys_who_played:
             player = dbutils.get_or_create(session, Player, username=teammate.contents[0])
-            player.games_played.append(game)
+            game_played = GamePlayed(player.id, game.id)
+            session.add(game_played)
         session.flush()
         session.commit()
+        Session.remove()
         pass
-    except:
+    except Exception, e:
         print('Game request failed for %s' % game_id)
+
+def update_monthly_games_played():
+    gc = gspread.login('xtremerunnerars@gmail.com', 'muatkienjwxfpnxn')
+    player_cell_locations = {'thewarmth00':'B51',
+                                'rob_chainsaw':'B53',
+                                'mashley93':'B55',
+                                'peachy36west':'B57',
+                                'm_sibs':'B59',
+                                'rc_van':'B61',
+                                'soviet_canuck':'B63',
+                                'undertheblanket':'B65',
+                                'vsmewen':'B67',
+                                'hokagesama1':'B69',
+                                'lnferno31':'H86'}
+    sheet = gc.open_by_key('0Ak-m4uT6aXL1dExuUHdLV0x2aTNFSGNRMTV2WWdLX2c').get_worksheet(9)
+    session = Session()
+    our_players = session.query(Player).all()
+    for player in our_players:
+        cell_location = player_cell_locations[player.username.lower()]
+        games_played = len(session.query(GamePlayed).filter_by(player_id=player.id).all())
+        value = sheet.acell(cell_location).value
+        sheet.update_acell(cell_location, str(games_played))
+        if sheet.acell(cell_location).value != value:
+            pass
+        else:
+            pass
+
+need_to_update_google_doc = check_games()
+if need_to_update_google_doc:
+    update_monthly_games_played()
+
--- models.py
+++ models.py
@@ -5,9 +5,10 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, backref, scoped_session
 from sqlalchemy.orm.session import sessionmaker
 from sqlalchemy.schema import ForeignKey
+from sqlalchemy.types import BigInteger
 import dbutils
 
-engine = create_engine('mysql://b847e1cb77ce40:df22c88c@us-cdbr-east-02.cleardb.com/heroku_13d98724def4930', echo=True, pool_recycle=3600)#recycle connection every hour to prevent overnight disconnect)
+engine = create_engine('mysql://b847e1cb77ce40:df22c88c@us-cdbr-east-02.cleardb.com/heroku_13d98724def4930', echo=False, pool_recycle=3600)#recycle connection every hour to prevent overnight disconnect)
 Base = declarative_base(bind=engine)
 sm = sessionmaker(bind=engine, autoflush=True, autocommit=False, expire_on_commit=False)
 Session = scoped_session(sm)
@@ -18,33 +19,34 @@ class Player(Base):
     id = Column(u'id', INTEGER(), primary_key=True, nullable=False)
     username = Column(u'username', VARCHAR(length=32), nullable=False)
 
+    def __init__(self, username):
+        self.username = username
+
 #    item_completions = relationship('ItemCompletion', backref='user') #one to many
-    games_played = association_proxy('games_played', 'game')
+    games = association_proxy('games_played', 'game')
 
 class GamePlayed(Base):
     __tablename__ = 'gameplayed'
 
-    user_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
+    player_id = Column(Integer, ForeignKey('player.id'), primary_key=True)
     game_id = Column(Integer, ForeignKey('game.id'), primary_key=True)
-    file_path = Column(VARCHAR(255))
 
-    user = relationship(Player, backref=backref("games_played", cascade="all, delete-orphan"))
+    player = relationship(Player, backref=backref("games_played", cascade="all, delete-orphan"))
 
     Game = relationship("Game")
 
-    def __init__(self, user_id, game_id, file_path=None):
-        self.user_id = user_id
+    def __init__(self, player_id=None, game_id=None):
+        self.player_id = player_id
         self.game_id = game_id
-        self.file_path = file_path
 
     def __repr__(self):
-        return '<Gameplayed %d @ %d>' % (self.user_id, self.match_id)
+        return '<Gameplayed %d @ %d>' % (self.player_id, self.match_id)
 
 
 class Game(Base):
     __tablename__ = 'game'
     id = Column(Integer, primary_key=True)
-    match_id = Column(Integer)
+    match_id = Column(BigInteger)
 
     def __init__(self, match_id):
         self.match_id = match_id
